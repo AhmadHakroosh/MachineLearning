898527a24ef234a9ce27ba524655da2257c1278d
Ahmad Hakroosh <ahmad@ahmad-h.com>
2019-06-15 13:38:40 +0300

    restored unsaved changes & more work

--- Data Preprocessing/data_preprocessing.py
+++ Data Preprocessing/data_preprocessing.py
@@ -13,7 +13,7 @@ import pandas as pd
 # Import dataset
 dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
-Y = dataset.iloc[:, -1].values
+y = dataset.iloc[:, -1].values
 
 # Taking care of missing data
 from sklearn.preprocessing import Imputer
@@ -28,3 +28,10 @@ label_encoder_X = LabelEncoder()
 X[:, 0] = label_encoder_X.fit_transform(X[:, 0])
 one_hot_encoder = OneHotEncoder(categorical_features = [0])
 X = one_hot_encoder.fit_transform(X).toarray()
+
+label_encoder_y = LabelEncoder()
+y = label_encoder_y.fit_transform(y)
+
+# Splitting the dataset into Train set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
